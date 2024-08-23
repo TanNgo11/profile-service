a9cae956678fd401e40db3d87c471d56acb07270
TanNgo11 <tan.ngo.cit20@eiu.edu.vn>
2024-08-23 09:01:22 +0700

    feat/update-profile-service

--- src/main/java/com/shadcn/profileservice/controller/UserProfileController.java
+++ src/main/java/com/shadcn/profileservice/controller/UserProfileController.java
@@ -1,18 +1,16 @@
 package com.shadcn.profileservice.controller;
 
-import java.util.List;
-
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-
 import com.shadcn.profileservice.dto.request.ProfileCreationRequest;
 import com.shadcn.profileservice.dto.response.ApiResponse;
 import com.shadcn.profileservice.dto.response.UserProfileResponse;
 import com.shadcn.profileservice.service.UserProfileService;
-
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
--- src/main/java/com/shadcn/profileservice/repository/UserProfileRepository.java
+++ src/main/java/com/shadcn/profileservice/repository/UserProfileRepository.java
@@ -1,9 +1,10 @@
 package com.shadcn.profileservice.repository;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import com.shadcn.profileservice.entity.UserProfile;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserProfileRepository extends MongoRepository<UserProfile, String> {}
+public interface UserProfileRepository extends Neo4jRepository<UserProfile, String> {
+}
