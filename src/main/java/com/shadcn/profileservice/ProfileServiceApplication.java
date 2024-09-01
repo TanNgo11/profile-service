5a9bea031722b84aeb95b84ad8797c6b10fcf7e6
TanNgo11 <tan.ngo.cit20@eiu.edu.vn>
2024-09-01 13:08:35 +0700

    fix/fix-conflict-rebase'

--- src/main/java/com/shadcn/profileservice/ProfileServiceApplication.java
+++ src/main/java/com/shadcn/profileservice/ProfileServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cache.annotation.EnableCaching;
 @SpringBootApplication
 @EnableCaching
 public class ProfileServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ProfileServiceApplication.class, args);
     }
