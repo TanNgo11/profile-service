3f3d2bacaec34b075b228847a1d4c4c4c6d7dbd3
thanhtrancit20 <thanh.tran.cit20@eiu.edu.vn>
2024-11-27 11:22:06 +0700

    filter

--- src/main/java/com/shadcn/profileservice/service/IAdminProfileService.java
+++ src/main/java/com/shadcn/profileservice/service/IAdminProfileService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.shadcn.profileservice.dto.request.*;
 import com.shadcn.profileservice.dto.response.*;
+import com.shadcn.profileservice.entity.AdminProfile;
 
 public interface IAdminProfileService {
 
@@ -18,4 +19,6 @@ public interface IAdminProfileService {
     List<AdminProfileResponse> getAllAdminProfilesByUsernames(String[] usernames);
 
     void updateAdminProfile(String id, UpdateAdminProfileRequest request);
+
+    List<AdminProfileResponse> filterAdmins(AdminFilterRequest filterRequest);
 }
